1683b4d87d5056ff6b784c032c190da92c32b4c9
Veronchi <zhigunova1995@gmail.com>
2022-01-14 17:36:06 +0300

    implement layout for sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,7 +5,39 @@ import { social, links } from './data'
 
 const Sidebar = () => {
   return (
-    <aside className='sidebar'></aside>
+    <aside className='sidebar'>
+      <div className='sidebar-header'>
+        <img src={logo} className='logo' alt='logo' />
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className='links'>
+        {
+          links.map(({ id, url, text, icon }) => {
+            return (
+              <li key={id}>
+                <a href={url}>
+                  {icon}
+                  {text}
+                </a>
+              </li>
+            )
+          })
+        }
+      </ul>
+      <ul className='social-icons'>
+        {social.map(({ id, url, icon }) => {
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
   )
 }
 
